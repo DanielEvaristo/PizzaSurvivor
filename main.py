0b5ee4db16121e904e8ffaa74b78c19222b308e4
DanielEvaristo <evaristo.camargo.daniel@gmail.com>
2026-07-28 09:40:33 -0600

    Titulo, Icono y Fondo

--- main.py
+++ main.py
@@ -5,6 +5,13 @@ pygame.init()
 
 #Crear pantalla de juego
 pantalla = pygame.display.set_mode((800,600))
+pygame.display.set_caption("Pizza Survivor")
+icono = pygame.image.load("pizza.png")
+pygame.display.set_icon(icono)
+
+#Fondo de juego
+fondo = pygame.image.load("fondo.png")
+fondo = pygame.transform.scale(fondo,(800,600))
 
 
 #Bucle principal del juego
@@ -13,7 +20,7 @@ while se_ejecuta:
     for evento in pygame.event.get():
         if evento.type == pygame.QUIT:
             se_ejecuta = False
-        pantalla.fill((230,220,240))
+        pantalla.blit(fondo,(0,0))
         pygame.display.update()
 
 pygame.quit()
